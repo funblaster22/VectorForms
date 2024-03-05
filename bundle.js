c812d54bc9177e56e4c184315c0762a77a0fa3d2
funblaster22 <53224922+funblaster22@users.noreply.github.com>
2024-03-05 03:16:55 +0000

    Deploying to gh-pages from @ funblaster22/VectorForms@65d2d2d5da2f3f1e08e35732dbcb3f9e62f17c1c 🚀

--- bundle.js
+++ bundle.js
@@ -34,7 +34,7 @@
 /******/ 	__webpack_require__.c = installedModules;
 
 /******/ 	// __webpack_public_path__
-/******/ 	__webpack_require__.p = "/formbuilder/";
+/******/ 	__webpack_require__.p = "/VectorForms";
 
 /******/ 	// Load entry module and return exports
 /******/ 	return __webpack_require__(0);
